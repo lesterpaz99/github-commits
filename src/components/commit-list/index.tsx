4aec114beef64e9aeeb85c4abdc2c4fd528a5c83
Obed Paz <62860279+lesterpaz99@users.noreply.github.com>
2023-10-17 16:18:01 -0600

    First commit

---
+++ src/components/commit-list/index.tsx
@@ -0,0 +1,13 @@
+// mock data for github commits
+const commits = [
+  { id: 1, message: 'feat: add commit list' },
+  { id: 2, message: 'fix: delete commit list' },
+  { id: 3, message: 'feat: add commit list' },
+  { id: 4, message: 'fix: update commit list' },
+]
+
+export const CommitList = () => {
+  return <ul>
+   
+  </ul>
+}
